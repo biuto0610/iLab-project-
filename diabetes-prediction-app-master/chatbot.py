558885325b3a3ad1c0b7f1f73ce6b552e2faa5eb
nipeshshrestha <113221247+nipeshshrestha@users.noreply.github.com>
2024-04-29 00:15:42 +1000

    Add files via upload

---
+++ diabetes-prediction-app-master/chatbot.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import openai
+from streamlit_chat import message
+
+openai.api_key = "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"
+
+def  bot():
+        def api_calling(prompt):
+                completions = openai.completions.create(
+                        
+                        model="gpt-3.5-turbo-instruct",
+                        prompt=prompt,
+                        max_tokens=1024,
+                        n=1,
+                        stop=None,
+                        temperature=0.5,
+                )
+                message = completions.choices[0].text
+                return message
+
+        st.title("DiabetesGPT")
+        if 'user_input' not in st.session_state:
+                st.session_state['user_input'] = []
+
+        if 'openai_response' not in st.session_state:
+                st.session_state['openai_response'] = []
+
+        def get_text():
+                input_text = st.text_input("your diabetes assistant", key="input")
+                return input_text
+
+        user_input = get_text()
+
+        if user_input:
+                output = api_calling(user_input)
+                output = output.lstrip("\n")
+
+                # Store the output
+                st.session_state.openai_response.append(user_input)
+                st.session_state.user_input.append(output)
+
+        message_history = st.empty()
+
+        if st.session_state['user_input']:
+                for i in range(len(st.session_state['user_input']) - 1, -1, -1):
+                        # This function displays user input
+                        message(st.session_state["user_input"][i], 
+                                        key=str(i),avatar_style="icons")
+                        # This function displays OpenAI response
+                        message(st.session_state['openai_response'][i], 
+                                        avatar_style="miniavs",is_user=True,
+                                        key=str(i) + 'data_by_user')
